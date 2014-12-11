12289aae5f13460da1d12c19e57f6c670f5906f1
Ryan Pavlik <ryan@sensics.com>
2014-12-11 12:18:07 -0600

    Add display parameter example

---
+++ Managed-OSVR/ExampleClients/DisplayParameter/DisplayParameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OSVR.ClientKit;
+
+namespace DisplayParameter
+{
+    class DisplayParameter
+    {
+        static void Main(string[] args)
+        {
+            OSVR.ClientKit.ClientContext context = new OSVR.ClientKit.ClientContext("org.opengoggles.exampleclients.managed.DisplayParameter");
+            string displayDescription = context.getStringParameter("/display");
+
+            Console.WriteLine("Got value of /display:");
+            Console.WriteLine(displayDescription);
+
+            Console.WriteLine("Library shut down; exiting.");
+        }
+    }
+}
