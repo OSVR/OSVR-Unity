59c724e3675c0d8d78122f48a6c478a073c72999
Ryan Pavlik <ryan@sensics.com>
2014-12-15 17:49:06 -0600

    Add build scripts.

---
+++ OSVR-Unity/Assets/Editor/OSVRUnityBuild.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using System.Collections;
+
+public class OSVRUnityBuild {
+
+	static void build() {
+		string[] assets = {
+			"Assets/OSVRUnity",
+			"Assets/Plugins"
+		};
+		AssetDatabase.ExportPackage(assets,
+		                            "OSVR-Unity.unitypackage",
+		                            ExportPackageOptions.IncludeDependencies | ExportPackageOptions.Recurse);
+		AssetDatabase.ExportPackage("Assets/_scenes/minigame.unity",
+		                            "OSVR-Unity-sample.unitypackage",
+		                            ExportPackageOptions.IncludeDependencies | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeLibraryAssets);
+	}
+}
---
+++ prep-package.cmd
@@ -0,0 +1,4 @@
+cd /d %~dp0
+
+mkdir OSVR-Unity-Dist
+xcopy Managed-OSVR OSVR-Unity-Dist /Y ?R
