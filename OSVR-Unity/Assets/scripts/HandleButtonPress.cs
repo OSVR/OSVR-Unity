cc7d4598664b8119a820406722962f9f03f5332b
Ryan Pavlik <ryan@sensics.com>
2014-12-15 13:46:26 -0600

    Add a button press example

---
+++ OSVR-Unity/Assets/scripts/HandleButtonPress.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleButtonPress : MonoBehaviour {
+	public OSVR.Unity.OSVRClientKit clientKit;
+	
+	public OSVR.Unity.InterfaceCallbacks cb;
+
+	public void Start() {
+		GetComponent<OSVR.Unity.InterfaceCallbacks> ().RegisterCallback (handleButton);
+	}
+
+	public void handleButton(string path, bool state) {
+		Debug.Log ("Got button: " + path + " state is " + state);
+	}
+
+}
