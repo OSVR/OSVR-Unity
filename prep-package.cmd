ae0cfc90df47148721367010a7747d2e8bbbf3d0
Ryan Pavlik <ryan@sensics.com>
2014-12-15 19:23:32 -0600

    Fix slashes in batch file.

--- prep-package.cmd
+++ prep-package.cmd
@@ -8,5 +8,5 @@ xcopy Managed-OSVR OSVR-Unity-Dist\src\Managed-OSVR /Y /S
 xcopy OSVR-Unity OSVR-Unity-Dist\src\OSVR-Unity /Y /S
 
 rem the -F 0x4 is to turn off smartypants.
-third-party/discount-2.1.6-win32/markdown.exe -F 0x4 -o README.html OSVR-Unity/README.md
+third-party\discount-2.1.6-win32\markdown.exe -F 0x4 -o README.html OSVR-Unity/README.md
 move README.html OSVR-Unity-Dist\
