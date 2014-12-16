e88f2709ab9345429c5803c2bb55144fd3b35365
Ryan Pavlik <ryan@sensics.com>
2014-12-15 19:21:53 -0600

    Convert the README file to HTML and include it in the bundle.

--- prep-package.cmd
+++ prep-package.cmd
@@ -6,3 +6,7 @@ mkdir OSVR-Unity-Dist\src\OSVR-Unity
 mkdir OSVR-Unity-Dist\src\Managed-OSVR
 xcopy Managed-OSVR OSVR-Unity-Dist\src\Managed-OSVR /Y /S
 xcopy OSVR-Unity OSVR-Unity-Dist\src\OSVR-Unity /Y /S
+
+rem the -F 0x4 is to turn off smartypants.
+third-party/discount-2.1.6-win32/markdown.exe -F 0x4 -o README.html OSVR-Unity/README.md
+move README.html OSVR-Unity-Dist\
