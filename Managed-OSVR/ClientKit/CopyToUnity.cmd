a40c3a3eb16d69e2716e9079c69116f65ade89f6
Ryan Pavlik <ryan@sensics.com>
2014-12-17 17:00:21 -0600

    Update CopyToUnity script.

--- Managed-OSVR/ClientKit/CopyToUnity.cmd
+++ Managed-OSVR/ClientKit/CopyToUnity.cmd
@@ -1,6 +1,14 @@
+rem For use as the following post-build command with command line arguments:
+rem $(ProjectDir)CopyToUnity.cmd "$(TargetPath)" "$(TargetDir)" $(PlatformName)
+
 echo on
 cd /d "%~dp0"
 
+rem Copy the managed wrapper DLL
 xcopy "%1" ..\..\OSVR-Unity\Assets\Plugins\%3\ /Y 
+
+rem Copy the native DLLs that the build process puts alongside the build product
 xcopy "%2osvr*.dll" ..\..\OSVR-Unity\Assets\Plugins\%3\ /Y 
-xcopy "%2msvc*.dll" ..\..\OSVR-Unity\Assets\Plugins\%3\ /Y 
+
+rem Copy the MSVC runtime redist libraries from alongside this script
+xcopy "msvc*.dll" ..\..\OSVR-Unity\Assets\Plugins\%3\ /Y 
