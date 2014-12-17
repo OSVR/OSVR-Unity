f720080a0b099b68f427aed0ff98c751eef26ac0
Ryan Pavlik <ryan@sensics.com>
2014-12-17 10:28:33 -0600

    Copy the redist files with the Managed-OSVR wrapper.

--- Managed-OSVR/ClientKit/CopyToUnity.cmd
+++ Managed-OSVR/ClientKit/CopyToUnity.cmd
@@ -3,3 +3,4 @@ cd /d "%~dp0"
 
 xcopy "%1" ..\..\OSVR-Unity\Assets\Plugins\%3\ /Y 
 xcopy "%2osvr*.dll" ..\..\OSVR-Unity\Assets\Plugins\%3\ /Y 
+xcopy "%2msvc*.dll" ..\..\OSVR-Unity\Assets\Plugins\%3\ /Y 
