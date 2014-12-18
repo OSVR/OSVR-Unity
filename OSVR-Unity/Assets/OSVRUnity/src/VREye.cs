39155c4f648c29ab15440e41bc44b8a9c54cd7ba
Ryan Pavlik <ryan@sensics.com>
2014-12-18 17:58:32 -0600

    Add HMD display prefabs, scripts, and samples from Bob.

---
+++ OSVR-Unity/Assets/OSVRUnity/src/VREye.cs
@@ -0,0 +1,65 @@
+﻿/* OSVR-Unity Connection
+ * 
+ * <http://sensics.com/osvr>
+ * Copyright 2014 Sensics, Inc.
+ * All rights reserved.
+ * 
+ * Final version intended to be licensed under Apache v2.0
+ */
+/// <summary>
+/// Author: Bob Berkebile
+/// Email: bob@bullyentertainment.com || bobb@pixelplacement.com
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public enum Eye{ left, right };
+
+public class VREye : MonoBehaviour
+{
+	#region Public Variables
+	public Eye eye;
+	[HideInInspector]
+	public Transform cachedTransform;
+	#endregion
+
+	#region Init
+	void Awake()
+	{
+		Init();
+	}
+	#endregion
+
+	#region Public Methods
+	public void MatchCamera( Camera sourceCamera )
+	{
+		camera.nearClipPlane = sourceCamera.nearClipPlane;
+		camera.farClipPlane = sourceCamera.farClipPlane;
+		camera.backgroundColor = sourceCamera.backgroundColor;
+		camera.clearFlags = sourceCamera.clearFlags;
+		camera.cullingMask = sourceCamera.cullingMask;
+	}
+	#endregion
+
+	#region Private Methods
+	void Init()
+	{
+		//cache:
+		cachedTransform = transform;
+
+		if ( camera == null ) gameObject.AddComponent<Camera>();
+
+		//camera setups:
+		switch( eye )
+		{
+		case Eye.left:
+			camera.rect = new Rect( 0, 0, .5f, 1 );
+			break;
+		case Eye.right:
+			camera.rect = new Rect( .5f, 0, .5f, 1 );
+			break;
+		}
+	}
+	#endregion
+}
---
+++ OSVR-Unity/Assets/OSVRUnity/src/VRHead.cs
@@ -0,0 +1,120 @@
+﻿/* OSVR-Unity Connection
+ * 
+ * <http://sensics.com/osvr>
+ * Copyright 2014 Sensics, Inc.
+ * All rights reserved.
+ * 
+ * Final version intended to be licensed under Apache v2.0
+ */
+/// <summary>
+/// Author: Bob Berkebile
+/// Email: bob@bullyentertainment.com || bobb@pixelplacement.com
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public enum ViewMode{ stereo, mono };
+
+[RequireComponent( typeof( Camera ) ) ]
+public class VRHead : MonoBehaviour
+{
+	#region Public Variables
+	public ViewMode viewMode;
+	[Range( 0, 1 )]
+	public float stereoAmount;
+	public float maxStereo = .03f;
+	#endregion
+
+	#region Private Variables
+	VREye _leftEye;
+	VREye _rightEye;
+	float _previousStereoAmount;
+	ViewMode _previousViewMode;
+	#endregion
+
+	#region Init
+	void Start()
+	{
+		Init();
+		CatalogEyes();
+	}
+	#endregion
+
+	#region Loop
+	void Update()
+	{
+		UpdateStereoAmount();
+		UpdateViewMode();
+	}
+	#endregion
+
+	#region Public Methods
+	#endregion
+
+	#region Private Methods
+	void UpdateViewMode()
+	{
+		if( Time.realtimeSinceStartup < 100 || _previousViewMode != viewMode )
+		{
+			switch( viewMode )
+			{
+			case ViewMode.mono:
+				camera.enabled = true;
+				_leftEye.camera.enabled = false;
+				_rightEye.camera.enabled = false;
+				break;
+
+			case ViewMode.stereo:
+				camera.enabled = false;
+				_leftEye.camera.enabled = true;
+				_rightEye.camera.enabled = true;
+				break;
+			}
+		}
+
+		_previousViewMode = viewMode;
+	}
+
+	void UpdateStereoAmount()
+	{
+		if( stereoAmount != _previousStereoAmount )
+		{
+			stereoAmount = Mathf.Clamp( stereoAmount, 0, 1 );
+			_rightEye.cachedTransform.localPosition = Vector3.right * ( maxStereo * stereoAmount );
+			_leftEye.cachedTransform.localPosition = Vector3.left * ( maxStereo * stereoAmount );
+			_previousStereoAmount = stereoAmount;
+		}
+	}
+
+	void CatalogEyes()
+	{
+		foreach( VREye currentEye in GetComponentsInChildren<VREye>() )
+		{
+			//match:
+			currentEye.MatchCamera( camera );
+
+			//catalog:
+			switch( currentEye.eye )
+			{
+			case Eye.left:
+				_leftEye = currentEye;
+				break;
+				
+			case Eye.right:
+				_rightEye = currentEye;
+				break;
+			}
+		}
+	}
+
+	void Init()
+	{
+		//VR should never timeout the screen:
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+		//60 FPS whenever possible:
+		Application.targetFrameRate = 60;
+	}
+	#endregion
+}
