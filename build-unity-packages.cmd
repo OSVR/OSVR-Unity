40f0555d2fbd946e0ffe59cc4b3def7d4bd6cbb7
Ryan Pavlik <ryan@sensics.com>
2015-01-21 08:41:08 -0600

    Fail the batch file if no unity package made.

--- build-unity-packages.cmd
+++ build-unity-packages.cmd
@@ -2,3 +2,4 @@ cd /d %~dp0
 "C:\Program Files (x86)\Unity\Editor\Unity.exe" -quit -batchmode -projectPath "%~dp0OSVR-Unity" -executeMethod OSVRUnityBuild.build
 
 xcopy OSVR-Unity\*.unitypackage OSVR-Unity-Dist /Y
+if not exist OSVR-Unity\*.unitypackage exit /B 1
