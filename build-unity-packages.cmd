e184d3a2f8ab4f8bf8c6dd2048536ad2823e87f9
Ryan Pavlik <ryan@sensics.com>
2015-02-20 11:31:37 -0600

    Improve behavior of `build-unity-packages.cmd`.
    Test for existence of some DLLs (prerequisite to a successful build),
    and only copy to the dist directory if it exists.

--- build-unity-packages.cmd
+++ build-unity-packages.cmd
@@ -1,5 +1,12 @@
 cd /d %~dp0
+
+rem Check to make sure we've actually copied over at least something that could be the OSVR and Managed-OSVR binaries.
+if not exist OSVR-Unity\Assets\Plugins\x86\*.dll exit /B 1
+
 "C:\Program Files (x86)\Unity\Editor\Unity.exe" -quit -batchmode -projectPath "%~dp0OSVR-Unity" -executeMethod OSVRUnityBuild.build
 
-xcopy OSVR-Unity\*.unitypackage OSVR-Unity-Dist /Y
+rem Fail the build if we didn't get a unitypackage out.
 if not exist OSVR-Unity\*.unitypackage exit /B 1
+
+rem Copy the unitypackage to the dist directory if it exists.
+if exist OSVR-Unity-Dist xcopy OSVR-Unity\*.unitypackage OSVR-Unity-Dist /Y
