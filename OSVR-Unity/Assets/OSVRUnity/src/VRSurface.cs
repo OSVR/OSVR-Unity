e67ebbcb05c9f34abdef108159d177bc2d640fe8
Greg Aring <greg.aring@gmail.com>
2015-08-18 18:38:46 -0400

    Added VRSurface.cs, which controls rendering for an eye.

---
+++ OSVR-Unity/Assets/OSVRUnity/src/VRSurface.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace OSVR
+{
+    namespace Unity
+    {
+        public class VRSurface : MonoBehaviour
+        {
+
+            private Camera _camera;
+            public Camera Camera { get { return _camera; } set { _camera = value; } }
+
+            public void SetViewport(Rect rect)
+            {
+                _camera.rect = rect;
+            }
+
+            public void SetProjectionMatrix(Matrix4x4 projMatrix)
+            {
+                _camera.projectionMatrix = projMatrix;
+            }
+
+            public void Render()
+            {
+                _camera.Render();
+            }
+
+        }
+    }
+}
