e21171bf525ca988aceaa5d38deda00e21d89250
Greg Aring <greg.aring@gmail.com>
2015-08-18 18:34:15 -0400

    Added utilities to convert Viewport Rects and Projection Matrices

--- OSVR-Unity/Assets/OSVRUnity/src/Math.cs
+++ OSVR-Unity/Assets/OSVRUnity/src/Math.cs
@@ -50,6 +50,35 @@ namespace OSVR
             {
                 return Matrix4x4.TRS(Math.ConvertPosition(pose.translation), Math.ConvertOrientation(pose.rotation), Vector3.zero);
             }
+
+            //@todo Rect takes left, top, width, height
+            public static Rect ConvertViewport(OSVR.ClientKit.Viewport viewport)
+            {
+                return new Rect(viewport.Left, viewport.Bottom + viewport.Height, viewport.Width, viewport.Height);
+            }
+
+            //@todo is this correct?
+            public static Matrix4x4 ConvertMatrix(OSVR.ClientKit.Matrix44f matrix)
+            {
+                Matrix4x4 matrix4x4 = new Matrix4x4();
+                matrix4x4.m00 = matrix.M0;
+                matrix4x4.m01 = matrix.M1;
+                matrix4x4.m02 = matrix.M2;
+                matrix4x4.m03 = matrix.M3;
+                matrix4x4.m10 = matrix.M4;
+                matrix4x4.m11 = matrix.M5;
+                matrix4x4.m12 = matrix.M6;
+                matrix4x4.m13 = matrix.M7;
+                matrix4x4.m20 = matrix.M8;
+                matrix4x4.m21 = matrix.M9;
+                matrix4x4.m22 = matrix.M10;
+                matrix4x4.m23 = matrix.M11;
+                matrix4x4.m30 = matrix.M12;
+                matrix4x4.m31 = matrix.M13;
+                matrix4x4.m32 = matrix.M14;
+                matrix4x4.m33 = matrix.M15;
+                return matrix4x4;
+            }
         }
     }
 }
