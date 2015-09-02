9a325ec0c926b841cc312d31a3d1536cd469e3c9
Greg Aring <greg.aring@gmail.com>
2015-09-02 18:19:14 -0400

    Added VRSurface[] and surfacecount variable. Added UpdateSurfaces and CreateSurfaces functions.

--- OSVR-Unity/Assets/OSVRUnity/src/VREye.cs
+++ OSVR-Unity/Assets/OSVRUnity/src/VREye.cs
@@ -32,9 +32,12 @@ namespace OSVR
     {
         public class VREye : MonoBehaviour
         {
-            #region Private Variables
-            private VRSurface _surface; //the surface associated with this eye
+            public const int NUM_SURFACES = 1;
+
+            #region Private Variables           
             private VRViewer _viewer; //the viewer associated with this eye
+            private VRSurface[] _surfaces; //the surfaces associated with this eye
+            private uint _surfaceCount;
             private uint _eyeIndex;
             
             #endregion
@@ -44,11 +47,8 @@ namespace OSVR
                 get { return _eyeIndex; }
                 set { _eyeIndex = value; }
             }
-            public VRSurface Surface
-            {
-                get { return _surface; }
-                set { _surface = value; }
-            }
+            public VRSurface[] Surfaces { get { return _surfaces; } } 
+            public uint SurfaceCount { get { return _surfaceCount; } }
             public VRViewer Viewer
             {
                 get { return _viewer; }
@@ -83,6 +83,85 @@ namespace OSVR
                 cachedTransform.localPosition = Math.ConvertPosition(eyePose.translation);
                 cachedTransform.localRotation = Math.ConvertOrientation(eyePose.rotation);
             }
+
+            //For each Surface, update viewing parameters and render the surface
+            public void UpdateSurfaces()
+            {
+                //for each surface
+                for (uint surfaceIndex = 0; surfaceIndex < SurfaceCount; surfaceIndex++)
+                {
+                    //get the eye's surface
+                    VRSurface surface = Surfaces[surfaceIndex];
+
+                    //get viewport from ClientKit and set surface viewport
+                    OSVR.ClientKit.Viewport viewport = Viewer.DisplayController.DisplayConfig.GetRelativeViewportForViewerEyeSurface(
+                        Viewer.ViewerIndex, (byte)_eyeIndex, surfaceIndex);
+
+                    surface.SetViewport(Math.ConvertViewport(viewport));
+
+                    //get projection matrix from ClientKit and set surface projection matrix
+                    OSVR.ClientKit.Matrix44f projMatrix = Viewer.DisplayController.DisplayConfig.GetProjectionMatrixForViewerEyeSurfacef(
+                        Viewer.ViewerIndex, (byte)_eyeIndex, surfaceIndex,
+                        surface.Camera.nearClipPlane, surface.Camera.farClipPlane, OSVR.ClientKit.MatrixConventionsFlags.ColMajor);
+
+                    surface.SetProjectionMatrix(Math.ConvertMatrix(projMatrix));
+
+                    //render the surface
+                    surface.Render();
+                }
+            }
+
+            //Create this Eye's VRSurfaces. 
+            //Each VRSurface has a camera component which controls rendering for the VREye
+            public void CreateSurfaces(uint surfaceCount)
+            {
+                _surfaceCount = surfaceCount;
+                _surfaces = new VRSurface[_surfaceCount];
+                if (surfaceCount != NUM_SURFACES)
+                {
+                    Debug.LogError("Eye" + _eyeIndex + " has " + surfaceCount + " surfaces, but " +
+                        "this implementation requires exactly one surface per eye.");
+                    return;
+                }
+                //loop through surfaces because at some point we could support eyes with multiple surfaces
+                //but this implementation currently supports exactly one
+                for (uint surfaceIndex = 0; surfaceIndex < surfaceCount; surfaceIndex++)
+                {
+                    GameObject surfaceGameObject = new GameObject("Surface");
+                    VRSurface surface = surfaceGameObject.AddComponent<VRSurface>();
+                    surface.Eye = this;
+                    surface.Camera = surfaceGameObject.GetComponent<Camera>(); //VRSurface has camera component by default
+                    CopyCamera(Viewer.DisplayController.Camera, surface.Camera); //copy camera properties from the "dummy" camera to surface camera
+                    surface.Camera.enabled = false; //disabled so we can control rendering manually
+                    surfaceGameObject.transform.parent = this.transform; //surface is child of Eye
+                    surfaceGameObject.transform.localPosition = Vector3.zero;
+                    Surfaces[surfaceIndex] = surface;
+
+                    //distortion
+                    bool useDistortion = Viewer.DisplayController.DisplayConfig.DoesViewerEyeSurfaceWantDistortion(Viewer.ViewerIndex, (byte)_eyeIndex, surfaceIndex);
+                    if(useDistortion)
+                    {
+                        //@todo figure out which type of distortion to use
+                        //right now, there is only one option, SurfaceRadialDistortion
+                        //get distortion parameters
+                        OSVR.ClientKit.RadialDistortionParameters distortionParameters =
+                        Viewer.DisplayController.DisplayConfig.GetViewerEyeSurfaceRadialDistortion(
+                        Viewer.ViewerIndex, (byte)_eyeIndex, surfaceIndex);
+
+                        surface.SetDistortion(distortionParameters);
+                    }                 
+                }
+            }
+
+            //helper method that copies camera properties from one camera to another
+            //copies from srcCamera to destCamera
+            private void CopyCamera(Camera srcCamera, Camera destCamera)
+            {
+                //Copy the camera properties.
+                destCamera.CopyFrom(srcCamera);
+
+                //@todo Copy other components attached to the DisplayController?
+            }           
         }
     }
 }
