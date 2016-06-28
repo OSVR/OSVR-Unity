493c11a91c56b3c2d4e76ad189e1246eb6a1c1d5
Greg Aring <greg@sensics.com>
2016-06-28 16:40:12 -0400

    Added empty scripts for DistortionMesh and DistortionParameters

---
+++ OSVR-Unity/Assets/OSVRUnity/src/OsvrDistortionMesh.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OSVR
+{
+    namespace Unity
+    {
+        public class OsvrDistortionMesh : MonoBehaviour
+        {
+
+            
+        }
+    }
+}
---
+++ OSVR-Unity/Assets/OSVRUnity/src/OsvrDistortionParameters.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OSVR
+{
+    namespace Unity
+    {
+        public class OsvrDistortionParameters : MonoBehaviour
+        {
+
+            // Use this for initialization
+            void Start()
+            {
+
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+
+            }
+        }
+    }
+}
