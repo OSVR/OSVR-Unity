83e99d35d78a128b26f2da86d763abf53944a6cd
Greg Aring <greg@sensics.com>
2017-09-18 21:06:45 -0400

    add editor util to Untiy 2017

--- OSVR-Unity/Assets/OSVRUnity/Editor/OsvrEditorUtils.cs
+++ OSVR-Unity/Assets/OSVRUnity/Editor/OsvrEditorUtils.cs
@@ -17,7 +17,7 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 /// </copyright>
-#if UNITY_5
+#if UNITY_5 || UNITY_2017
 using UnityEngine;
 using UnityEditor;
 using System.IO;
